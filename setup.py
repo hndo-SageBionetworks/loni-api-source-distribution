5afa1bb14afe6b5b4137c1f78438a3ba251ebdb4
EC2 Default User <ec2-user@ip-10-41-22-108.ec2.internal>
2022-07-18 20:00:29 +0000

    Created source distribution

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(name='loni-api',
+      version='0.1',
+      description='Package to make calls to the LONI API',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+      ],
+      keywords='sage bionetworks ida file downloads',
+      url='https://github.com/Sage-Bionetworks/loni-api',
+      author='Hieu Do',
+      author_email='hieu.do@sagebionetworks.org',
+      license='MIT',
+      packages=['loni-api'],
+      install_requires=[
+          'requests', 'configparser', 'os', 'pandas', 'mimetypes'
+      ],
+      zip_safe=False)
